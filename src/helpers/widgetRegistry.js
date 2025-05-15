44380667ffdf243ef99d590ec8d2430dbde3581d
vikashsingh <vikash.singh1@timesgroup.com>
2025-05-15 15:08:53 +0530

    json and api name correct and add header and footer

--- src/helpers/widgetRegistry.js
+++ src/helpers/widgetRegistry.js
@@ -1,7 +1,7 @@
 // helpers/widgetRegistry.js
-import Widget1 from "@/components/POC/Component/Widget1/Widget1";
-import Widget2 from "@/components/POC/Component/Widget2/Widget2";
-import Widget3 from "@/components/POC/Component/Widget3/widget3";
+import Widget1 from "@/components/POC/Component/widget1/widget1";
+import Widget2 from "@/components/POC/Component/widget2/widget2";
+import Widget3 from "@/components/POC/Component/widget3/widget3.js";
 import HeaderWidget from "@/components/POC/HeaderWidget/HeaderWidget";
 import FooterWidget from "@/components/POC/FooterWidget/FooterWidget";
 import RHSWidget from "@/components/POC/RHSWidget/RHSWidget";
